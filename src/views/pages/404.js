f06d81e542a42d020a60e495dee9eaba7a32bc7e
shiningWang <weiyuwuchang@gmail.com>
2021-11-17 15:44:47 +0800

    404 page

--- src/views/pages/404.js
+++ src/views/pages/404.js
@@ -1,18 +1,24 @@
 import App from './../../App'
-import {html, render } from 'lit-html'
+import { html, render } from 'lit-html'
+import { gotoRoute, anchorRoute } from "./../../Router"
 
-class FourOFourView{
-  init(){
-    console.log('FourOFourView.init')    
-    document.title = '404 File not found'    
+class FourOFourView {
+  init() {
+    console.log('FourOFourView.init')
+    document.title = '404 File not found'
     this.render()
   }
 
-  render(){
+  render() {
     const template = html`    
       <div class="calign">
         <h1>Opps!</h1>
         <p>Sorry, we couldn't find that.</p>
+        <button
+          class="return-home-btn"
+          @click="${() => gotoRoute("/")}"
+        >HOME
+        </button>
       </div>
     `
     render(template, App.rootEl)
